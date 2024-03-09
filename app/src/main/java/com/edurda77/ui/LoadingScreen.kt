643e8154ada052c56a3e83f54b5f1683c88de222
HayarovEd <edurd@rambler.ru>
2024-03-09 18:11:41 +0500

    v1_2

--- app/src/main/java/com/edurda77/ui/LoadingScreen.kt
+++ app/src/main/java/com/edurda77/ui/LoadingScreen.kt
@@ -3,12 +3,19 @@ package com.edurda77.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.edurda77.R
-
+import com.edurda77.ui.theme.orange
+@Preview
 @Composable
 fun LoadingScreen(
     modifier: Modifier = Modifier
@@ -22,6 +29,11 @@ fun LoadingScreen(
             contentDescription = "",
             contentScale = ContentScale.FillBounds
         )
-
+        CircularProgressIndicator(
+            modifier = modifier
+                .align(alignment = Alignment.Center)
+                .size(135.dp),
+            color = orange
+        )
     }
 }
